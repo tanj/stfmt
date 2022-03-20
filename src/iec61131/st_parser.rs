47b25096223b39201ae283515c1f187ab0278571
John te Bokkel <john@powercoreeng.com>
2022-03-20 16:12:57 -0700

    more tests

--- src/iec61131/st_parser.rs
+++ src/iec61131/st_parser.rs
@@ -10,53 +10,81 @@ mod tests {
     use super::*;
     use pest::{consumes_to, parses_to};
 
-    #[test]
-    fn test_numeric_16_123() {
-        parses_to! {
-            parser: StParser,
-            input: "16#123",
-            rule: Rule::numeric,
-            tokens: [numeric(0, 6)]
-        };
+    mod numeric_rule {
+        use super::*;
+
+        #[test]
+        fn test_hex_123() {
+            parses_to! {
+                parser: StParser,
+                input: "16#123",
+                rule: Rule::numeric,
+                tokens: [numeric(0, 6)]
+            };
+        }
+
+        #[test]
+        fn test_hex_with_underscore() {
+            parses_to! {
+                parser: StParser,
+                input: "16#dead_BEEF",
+                rule: Rule::numeric,
+                tokens: [numeric(0, 12)]
+            };
+        }
+
+        #[test]
+        fn test_octaly() {
+            parses_to! {
+                parser: StParser,
+                input: "8#123",
+                rule: Rule::numeric,
+                tokens: [numeric(0, 5)]
+            };
+        }
+
+        #[test]
+        fn test_binary() {
+            parses_to! {
+                parser: StParser,
+                input: "2#1010_1010_",
+                rule: Rule::numeric,
+                tokens: [numeric(0, 12)]
+            };
+        }
+
+        #[test]
+        fn test_decimal() {
+            parses_to! {
+                parser: StParser,
+                input: "10#123_456",
+                rule: Rule::numeric,
+                tokens: [numeric(0, 10)]
+            };
+        }
     }
 
-    #[test]
-    fn test_numeric_16_with_underscore() {
-        parses_to! {
-            parser: StParser,
-            input: "16#dead_BEEF",
-            rule: Rule::numeric,
-            tokens: [numeric(0, 12)]
-        };
-    }
+    mod string_rule {
+        use super::*;
 
-    #[test]
-    fn test_numeric_8() {
-        parses_to! {
-            parser: StParser,
-            input: "8#123",
-            rule: Rule::numeric,
-            tokens: [numeric(0, 5)]
-        };
-    }
+        #[test]
+        fn test_no_escapes() {
+            parses_to! {
+                parser: StParser,
+                input: "'Hello, world!'",
+                rule: Rule::string,
+                tokens: [string(0, 15)]
+            };
+        }
 
-    #[test]
-    fn test_numeric_2() {
-        parses_to! {
-            parser: StParser,
-            input: "2#1010_1010_",
-            rule: Rule::numeric,
-            tokens: [numeric(0, 12)]
-        };
-    }
-
-    #[test]
-    fn test_numeric_10() {
-        parses_to! {
-            parser: StParser,
-            input: "10#123_456",
-            rule: Rule::numeric,
-            tokens: [numeric(0, 10)]
-        };
+        #[test]
+        fn test_escapes() {
+            parses_to! {
+                parser: StParser,
+                input: "'$0a$0A$n$N$r$R$l$L$p$P$t$T$$$''",
+                rule: Rule::string,
+                tokens: [string(0, 32)]
+            };
+        }
     }
 }
